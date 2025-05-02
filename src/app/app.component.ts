3cb703201d1804b24a1a2b2a98d2f43480e39f1a
Giacomo Barbalinardo <info@ready24it.eu>
2025-05-02 12:57:57 +0200

    fix: add missing event click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CalenderEventInterface } from '../../projects/ngx-calender-widget/src/lib/calender-event.interface';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: false,
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ngx-calender-widget';
+    title = 'ngx-calender-widget';
+    date = new Date();
+
+    events = [
+        {
+            id: 1,
+            title: 'Event 1',
+            date: this.date.toISOString(),
+            endDate: null,
+        }
+    ] as CalenderEventInterface[];
 }
